5cebcd3c8bd44b79a0ef03cfc144072ccd152bd9
PatrickTey <93598645+PatrickTey@users.noreply.github.com>
2022-03-11 04:39:10 +0000

    second

---
+++ main.js
@@ -0,0 +1,13 @@
+let item = document.getElementById('mail');
+let touch = document.getElementById('trigger');
+
+function show() {
+    if (item.style.display === 'none') {
+        item.style.display = 'block'
+    } else {
+        item.style.display = 'none'
+    }
+}
+
+touch.addEventListener('click', show);
+
